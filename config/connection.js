31282753818abed538b82341605f0bcf32df6c39
Nardin Lachowski <nardin@live.com>
2021-02-09 22:13:49 -0500

    adding JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 //the code to connect Node to MySQL.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+const connection;
+
+if ( process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
+
 
 connection.connect((err) => {
     if(err) {
